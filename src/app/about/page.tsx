e16da292ffe119aba81013a91a8b97bd94034a61
mohamad massoidi <mohamadmassoudi90@gmail.com>
2025-09-01 10:08:03 +0330

    meta dats created and icon.ico created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'about',
+}
+ 
+
+function About() {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Button } from "@heroui/button";
 import Image from "next/image";
 import Link from "next/link";
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Home',
+}
+ 
+// Output: <title>About | Acme</title>
+// Output: <title>About | Acme</title>
+// Output: <title>About | Acme</title>
 
 export default function Home() {
   return (
     <div >
-      <Button>
-        <Link href="ai-palce">هوشمند سازی مکان</Link>
-      </Button>
+      
     </div>
   );
 }
